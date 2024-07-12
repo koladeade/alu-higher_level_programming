09ab2858741351c36a4dbf16ad206391f3783685
koladeade <k.adepoju@alustudent.com>
2024-07-12 03:57:38 -0700

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    define a class 'Square'
+"""
+
+
+class Square:
+    """
+        square with private instance attribute: 'size'
+    """
+
+    def __init__(self, size):
+        """
+            Args:
+                size (int): size of the new square
+        """
+        self.__size = size
