63a1ae01b8329d11dd00c624440524310ce48cde
koladeade <k.adepoju@alustudent.com>
2024-09-25 11:47:04 -0700

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 const myArgs = process.argv.slice(2);
 if (myArgs.length === 0) {
   console.log('No argument');
-}else if (myArgs.length === 1) {
-	console.log('Argument found');
-}else {
-	console.log('Argument found');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
